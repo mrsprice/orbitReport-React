b12baaf487a782355e5bd9cbb2d8413bc35405ef
mrsprice <simanowitz93@gmail.com>
2024-03-17 14:51:15 -0500

    problems a plenty

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
