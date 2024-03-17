17a18359896659e48cb09076b903c343a3dc386f
mrsprice <simanowitz93@gmail.com>
2024-03-17 14:35:45 -0500

    im, having issues

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,26 @@
+import "./styling.css";
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+        {sat.map((satellite, index) => (
+          <tr key={index}>
+            <td>{satellite.name}</td>
+            <td>{satellite.type}</td>
+            <td>{satellite.launchDate}</td>
+            <td>{satellite.operational ? 'Active' : 'Inactive'}</td>
+          </tr>
+        ))}
+      </tbody>
    </table>
   );
 };
